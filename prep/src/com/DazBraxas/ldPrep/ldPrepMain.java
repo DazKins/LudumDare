3c8eea641fd1af4c39a419f203a21fd156e67e81
LuciferAbraxas <theluciferabraxas@gmail.com>
2014-03-12 13:10:38 +0000

    added test syso line

--- prep/src/com/DazBraxas/ldPrep/ldPrepMain.java
+++ prep/src/com/DazBraxas/ldPrep/ldPrepMain.java
@@ -7,7 +7,12 @@ public class ldPrepMain extends Canvas implements Runnable {
 	private static final long serialVersionUID = 1L;
 
 	public static void main(String[] arg0){
-		
+		System.out.println("Test Test Test Test");
+		try {
+			Thread.sleep(5);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public ldPrepMain(){
