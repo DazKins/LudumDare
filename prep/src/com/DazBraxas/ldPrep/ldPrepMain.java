bee86fdaa80ce79b00f0cc8903f457ffc3fe6173
LuciferAbraxas <theluciferabraxas@gmail.com>
2014-03-12 13:06:28 +0000

    Added some prep files

---
+++ prep/src/com/DazBraxas/ldPrep/ldPrepMain.java
@@ -0,0 +1,21 @@
+package com.DazBraxas.ldPrep;
+
+import java.awt.Canvas;
+
+public class ldPrepMain extends Canvas implements Runnable {
+
+	private static final long serialVersionUID = 1L;
+
+	public static void main(String[] arg0){
+		
+	}
+	
+	public ldPrepMain(){
+		
+	}
+	
+	public void run(){
+		
+	}
+	
+}
