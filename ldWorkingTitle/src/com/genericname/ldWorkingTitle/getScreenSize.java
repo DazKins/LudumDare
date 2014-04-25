1cf721ced19a2095e04572b94ba19cf4f1f2a219
Morgan Hickman <theluciferabraxas@gmail.com>
2014-04-25 10:23:40 +0100

    the pile of arse that is github

--- ldWorkingTitle/src/com/genericname/ldWorkingTitle/getScreenSize.java
+++ ldWorkingTitle/src/com/genericname/ldWorkingTitle/getScreenSize.java
@@ -1,18 +1,25 @@
 package com.genericname.ldWorkingTitle;
 
+import java.awt.Dimension;
 import java.awt.DisplayMode;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
 
 public class getScreenSize {
 
-	public static void ScreenSize() {
+	public static Dimension[] ScreenSize() {
 		GraphicsDevice gd[] = GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices();
+		Dimension[] ss = new Dimension[2];
+		int height;
+		int width;
+		Dimension dim;
 		for (int i = 0; i < 2; i++) {
 			DisplayMode dm = gd[i].getDisplayMode();
-			Main.Width[i] = dm.getWidth();
-			Main.Height[i] = dm.getHeight();
+			width = dm.getWidth();
+			height = dm.getHeight();
+			dim = new Dimension(width, height);
+			ss[i] = dim;
 		}
-
+		return ss;	
 	}
 }
