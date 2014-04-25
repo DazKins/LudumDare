f4943686a1e40a176db4d36f561c1616792b2eff
DazKins <gamecake1@gmail.com>
2014-04-25 12:47:27 +0100

    RANDOMIZED PIXELS :D

--- src/gfx/Bitmap.java
+++ src/gfx/Bitmap.java
@@ -1,3 +1,8 @@
+package gfx;
+
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+import java.util.Random;
 
 public class Bitmap {
 	public int pixels[];
@@ -5,6 +10,22 @@ public class Bitmap {
 	
 	public Bitmap(int w, int h) {
 		pixels = new int[w * h];
-		
+		this.w = w;
+		this.h = h;
+		for (int i = 0; i < w * h; i++) {
+			pixels[i] = new Random().nextInt(0xFFFFFF);
+		}
+	}
+	
+	public Bitmap(BufferedImage img) {
+		w = img.getWidth();
+		h = img.getHeight();
+		pixels = ((DataBufferInt)img.getRaster().getDataBuffer()).getData();
+	}
+	
+	public void randomize() {
+		for (int i = 0; i < w * h; i++) {
+			pixels[i] = new Random().nextInt(0xFFFFFF);
+		}
 	}
 }
