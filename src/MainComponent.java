1a9af7c5600d9c841cf9fc5a7a23564ede1318b9
Morgan Hickman <theluciferabraxas@gmail.com>
2014-04-25 10:53:30 +0100

    added the canvas' and JFrames

--- src/MainComponent.java
+++ src/MainComponent.java
@@ -1,3 +1,5 @@
+import java.awt.BorderLayout;
+import java.awt.Canvas;
 import java.awt.Dimension;
 import java.awt.DisplayMode;
 import java.awt.GraphicsDevice;
@@ -9,12 +11,40 @@ public class MainComponent {
 	private JFrame j2;
 	
 	
+import javax.swing.JFrame;
+
+public class MainComponent implements Runnable{
+	
+	private static final String NAME = "Game";
+	private Canvas[] canvas = new Canvas[2];
+	private JFrame[] frame = new JFrame[2];
 	
 	public static void main(String args[]) {
 		System.out.println("test");
+		
+	}
+	
+	public MainComponent(){
+		for (int i = 0; i < 2; i++){
+			canvas[i].setMinimumSize(ScreenSize()[i]);
+			canvas[i].setMaximumSize(ScreenSize()[i]);
+			canvas[i].setPreferredSize(ScreenSize()[i]);
+			
+			frame[i] = new JFrame(NAME + ": Window " + i);
+			frame[i].setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame[i].setLayout(new BorderLayout());
+			
+			frame[i].add(canvas[i], BorderLayout.CENTER);
+			frame[i].pack();
+			
+			frame[i].setResizable(false);
+			frame[i].setLocationRelativeTo(null);
+			frame[i].setVisible(true);
+			
+		}
 	}
 
-	public static Dimension[] ScreenSize() {
+	private static Dimension[] ScreenSize() {
 		GraphicsDevice gd[] = GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices();
 		Dimension[] ss = new Dimension[2];
 		int height;
@@ -29,6 +59,11 @@ public class MainComponent {
 		}
 		return ss;
 
+	}
+	
+	public void run() {
+		
+		
 	}
 
 }
