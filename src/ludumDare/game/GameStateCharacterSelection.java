dc8985a67329ae4aec2c8cd02d1eb8d532cf06e5
DazKins <gamecake1@gmail.com>
2014-04-26 13:07:55 +0100

    sdasdasd

---
+++ src/ludumDare/game/GameStateCharacterSelection.java
@@ -0,0 +1,15 @@
+package ludumDare.game;
+
+import java.awt.Dimension;
+
+import ludumDare.MainComponent;
+import ludumDare.input.InputHandler;
+
+public class GameStateCharacterSelection extends GameState {
+
+	public GameStateCharacterSelection(MainComponent m, InputHandler i,
+			Dimension[] ws) {
+		super(m, i, ws);
+	}
+	
+}
