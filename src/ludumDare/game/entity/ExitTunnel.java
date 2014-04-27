f3dd0510c965c27652a9130a7c4c80eedad7f621
Morgan Hickman <theluciferabraxas@gmail.com>
2014-04-27 11:37:02 +0100

    exit pipe works added success and pipe open sound

--- src/ludumDare/game/entity/ExitTunnel.java
+++ src/ludumDare/game/entity/ExitTunnel.java
@@ -6,46 +6,77 @@ import ludumDare.gfx.Art;
 import ludumDare.gfx.Bitmap;
 import ludumDare.math.AABB;
 
-public class ExitTunnel extends Entity implements ActivateableEntity{
+public class ExitTunnel extends AOESwitch implements ActivateableEntity {
+	Audio ding = new Audio("/success.wav");
+	Audio openExit = new Audio("/openexit.wav");
 	
-	private boolean open = false;
-	
-	public ExitTunnel(int x, int y) {
-		super(x, y);
+	private int ticksSinceLastActivate;
+	private static boolean open = false;
+	public boolean success = false;
+
+	public ExitTunnel(float x, float y) {
+		super(new AABB(x, 23 + y, 16 + x, 24 + y));
+		this.x = x;
+		this.y = y;
 	}
-	
-	
+
+	public void onCollide(Entity e) {
+		super.onCollide(e);
+		ticksSinceLastActivate = lifeTicks;
+	}
+
 	public void onActivate(Entity e) {
 		open = !open;
-		
+		openExit.play(true);
+
 	}
-	
+
 	public AABB getAABB() {
 		if (!open)
 			return new AABB(x, y, 16 + x, 24 + y);
-		return null;
+		//trigger point for success
+		return new AABB(x, y + 23, 16 + x, 24 + y);
 	}
 
-	public boolean mayPassX(){
-		return false;
-	}
-	
-	public boolean mayPassY(Entity e) {
-		if (e instanceof Mob) 
+	public boolean mayPass(Entity e) {
+		if (open) {
+			//allows player to fall unhindered when open, else collision halts movement at trigger point
+			return true;
+		} else {
 			return false;
-		return true;
+		}
 	}
-	
-	
+
+	public void tick() {
+		super.tick();
+		if (open && !success) {
+			if (lifeTicks - ticksSinceLastActivate > 1 && activated) {
+				activated = false;
+			}
+			if (activated) {
+				success = true;
+				ding.play(true);
+				System.out.println("Success!");
+			}
+		}
+	}
+
 	public void render(Bitmap b, float xOff, float yOff) {
 		int state = open ? 16 : 18;
+		//column 1
 		b.blit((int) (x - xOff), (int) (y - yOff), Art.sprites[state][2], false, false, 1.0f, 1.0f);
 		b.blit((int) (x - xOff), (int) (y - yOff) + 8, Art.sprites[state][3], false, false, 1.0f, 1.0f);
 		b.blit((int) (x - xOff), (int) (y - yOff) + 16, Art.sprites[state][4], false, false, 1.0f, 1.0f);
-		b.blit((int) (x - xOff)+8, (int) (y - yOff), Art.sprites[state+1][2], false, false, 1.0f, 1.0f);
-		b.blit((int) (x - xOff)+8, (int) (y - yOff) +8, Art.sprites[state+1][3], false, false, 1.0f, 1.0f);
-		b.blit((int) (x - xOff)+8, (int) (y - yOff) + 16, Art.sprites[state+1][4], false, false, 1.0f, 1.0f);
-		
+		b.blit((int) (x - xOff), (int) (y - yOff) + 24, Art.sprites[state][5], false, false, 1.0f, 1.0f);
+		b.blit((int) (x - xOff), (int) (y - yOff) + 32, Art.sprites[state][6], false, false, 1.0f, 1.0f);
+		b.blit((int) (x - xOff), (int) (y - yOff) + 40, Art.sprites[state][7], false, false, 1.0f, 1.0f);
+		//column 2
+		b.blit((int) (x - xOff) + 8, (int) (y - yOff), Art.sprites[state + 1][2], false, false, 1.0f, 1.0f);
+		b.blit((int) (x - xOff) + 8, (int) (y - yOff) + 8, Art.sprites[state + 1][3], false, false, 1.0f, 1.0f);
+		b.blit((int) (x - xOff) + 8, (int) (y - yOff) + 16, Art.sprites[state + 1][4], false, false, 1.0f, 1.0f);
+		b.blit((int) (x - xOff) + 8, (int) (y - yOff) + 24, Art.sprites[state + 1][5], false, false, 1.0f, 1.0f);
+		b.blit((int) (x - xOff) + 8, (int) (y - yOff) + 32, Art.sprites[state + 1][6], false, false, 1.0f, 1.0f);
+		b.blit((int) (x - xOff) + 8, (int) (y - yOff) + 40, Art.sprites[state + 1][7], false, false, 1.0f, 1.0f);
 	}
-	
+
 }
